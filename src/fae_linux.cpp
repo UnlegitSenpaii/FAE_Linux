3ec9e4533dbfb7d420bda5e628a1c0b607be5b15
UnlegitSenpaii <39647545+UnlegitSenpaii@users.noreply.github.com>
2024-12-02 22:46:50 +0100

    Update fae_linux.cpp
    Signed-off-by: UnlegitSenpaii <39647545+UnlegitSenpaii@users.noreply.github.com>

--- src/fae_linux.cpp
+++ src/fae_linux.cpp
@@ -10,7 +10,7 @@
  * Patterns:
  * SteamContext::unlockAchievementsThatAreOnSteamButArentActivatedLocally jz > jnz
  * 74 37 66 0f 1f 44 00 00 48 8b 10 80
- * if this one breaks, try 74 37 66 0F 1F 44 00 00 48 8B ?? 80 7A ?? 00
+ * if this one breaks, look at the first match for 74 ? 66 0F 1F 44 ? ? 48 8B ? 80 7A ? 00
  * 
  * SteamContext::updateAchievementStatsFromSteam jz > jnz
  * 74 30 0f 1f 80 00 00 00 00 48 8b 10
